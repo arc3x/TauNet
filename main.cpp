16f18357d6737dfee2fdb4726b7a1af179f7d3f2
Matthew Slocum <arco000@gmail.com>
2015-12-02 01:01:46 +0000

    added initialization for secComponent added a binding that links secComponent to the msgComponent

--- main.cpp
+++ main.cpp
@@ -13,18 +13,20 @@ boost
 #include "regComponent.h"
 #include "uiComponent.h"
 #include "msgComponent.h"
+#include "secComponent.h"
 
 using namespace std;
 
 msgComponent * msg = new msgComponent();
 regComponent * reg = new regComponent();
+secComponent * reg = new secComponent();
+uiComponent * ui= new uiComponent();
 
 int main(int argc, char *argv[]) {
 	msg->bind_regComponent(reg);
-	reg->bind_msgComponent(msg);
-	
-	uiComponent * ui= new uiComponent();
-		ui->bind_regComponent(reg);
+    msg->bind_secComponent(sec);
+	reg->bind_msgComponent(msg);	
+    ui->bind_regComponent(reg);
 
 	if(argc > 1 && string(argv[1])=="-msg") {		
 		msg->run();
