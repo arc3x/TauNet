2d7ca4a8d3fda39564d7f7f182679166da8aaeec
Matthew Slocum <arco000@gmail.com>
2015-12-05 09:12:33 +0000

    added a test vector (it doesnt pass)

--- secComponent.test.cpp
+++ secComponent.test.cpp
@@ -2,10 +2,11 @@
 #include <cstring>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int test(secComponent * sec, string message, string key, int r, char * ciphertext, char * plaintext) {
+int test_encrypt_decrypt_inverting(secComponent * sec, string message, string key, int r, char * ciphertext, char * plaintext) {
     
     int ciphertext_len = sec->encrypt(message, key, r, ciphertext);
     int plaintext_len = sec->decrypt(ciphertext, message.length()+10, key, r, plaintext);
@@ -30,6 +31,12 @@ int test(secComponent * sec, string message, string key, int r, char * ciphertex
     return 1;
 }
 
+void zero(char * ar, int len) {
+    for (int i=0; i<len; i++) {
+        ar[i] = 0;
+    }
+}
+
 int main(void) {
     secComponent * sec = new secComponent();
     int r = 20;
@@ -37,11 +44,18 @@ int main(void) {
     char plaintext[1000];
     string key = "password";
     string message = "";
+   
+    
+    //
+    //'Unit' test for the encryption / decryption protocols
+    //
     
     //test 1
     //encrypt+decrypt on a single letter message
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
     message = "X";        
-    if(test(sec, message, key, r, ciphertext, plaintext)) {
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
        cout << "Pass" << endl; 
     } else {
         cout << "secComponent.test Test 1 failed" << endl;
@@ -49,8 +63,10 @@ int main(void) {
     
     //test 2
     //encrypt+decrypt on a single word message 
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
     message = "hello";
-    if(test(sec, message, key, r, ciphertext, plaintext)) {
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
        cout << "Pass" << endl; 
     } else {
         cout << "secComponent.test Test 2 failed" << endl;
@@ -58,8 +74,10 @@ int main(void) {
     
     //test 3
     //encrypt+decrypt on a multi word message 
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
     message = "hello world";
-    if(test(sec, message, key, r, ciphertext, plaintext)) {
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
        cout << "Pass" << endl; 
     } else {
         cout << "secComponent.test Test 3 failed" << endl;
@@ -68,13 +86,86 @@ int main(void) {
     
     //test 4
     //encrypt+decrypt on a multi word message with special characters
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
     message = "hello world!";
-    if(test(sec, message, key, r, ciphertext, plaintext)) {
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
        cout << "Pass" << endl;  
     } else {
         cout << "secComponent.test Test 4 failed" << endl;
     }
     
-    return 1;
+    //test 5
+    //encrypt+decrypt on a multi line message 
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
+    message = "hello world\r\nfish\r\n\r\nsalmon\r\n";
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
+       cout << "Pass" << endl;  
+    } else {
+        cout << "secComponent.test Test 5 failed" << endl;
+    }
+    
+    //test 6
+    //encrypt+decrypt on a multi line message with special characters
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
+    message = "!@#$%%^&*\0x94hello world!\r\nfish\r\n\r\ns@lmon;\r\n";
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
+       cout << "Pass" << endl;  
+    } else {
+        cout << "secComponent.test Test 6 failed" << endl;
+    }
+    
+    //test 6
+    //encrypt+decrypt on a multi line message with special characters
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
+    message = string("version: 0.2\r\n") 
+                        + "from: " + "source_user" + "\r\n"
+                        + "to: " + "dest_user" + "\r\n"
+                        + "\r\n"
+                        + "hey look a message!" + "\r\n";
+                        
+    if(test_encrypt_decrypt_inverting(sec, message, key, r, ciphertext, plaintext)) {
+       cout << "Pass" << endl;  
+    } else {
+        cout << "secComponent.test Test 7 failed" << endl;
+    }
+    
+    
+    //
+    //Decrytion tests using known encrypted values
+    //
+    cout << "Decryption tests using known good values: " << endl;
+    
+    //test 7
+    //decrypt cstest1.cs1 
+    //Key "asdfg".
+    //Plaintext will be "This is a test of CipherSaber." with no newline. 
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
+    key="asdfg";
+    ifstream cipherFile;
+    cipherFile.open("tst/cstest1.cs1");
+    
+    char c;
+    int i=0;
+    char ciphertext2 [1000];
+    if (cipherFile.is_open()) {
+        while (!cipherFile.eof()) {
+            //cipherFile >> ciphertext;
+            ciphertext[i] = cipherFile.get();
+            ciphertext2[i++] = cipherFile.get();
+        }
+        cout<<ciphertext << endl << endl;
+        cout<<ciphertext2;
+    }
+    cipherFile.close();
+    int plaintext_len = sec->decrypt(ciphertext, message.length()+10, key, r, plaintext);
+    
+    
+    
+    return 0;
 }
 
