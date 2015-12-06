46d956ef3b58a071d2dd188b2d63aafa2e1653b2
Matthew Slocum <arco000@gmail.com>
2015-12-06 01:10:34 +0000

    removed commented code

--- msgComponent.cpp
+++ msgComponent.cpp
@@ -137,15 +137,7 @@ void msgComponent::run() {
                 boost::asio::io_service io_service;
                 boost::asio::ip::tcp::resolver resolver(io_service);
                 boost::asio::ip::tcp::resolver::query query(reg->dest_list[reg->dest], "6283");
-                //XXXbelow is some crap REVISE IT!
-                //for(boost::asio::ip::tcp::resolver::iterator i = resolver.resolve(query);
-                //            i != boost::asio::ip::tcp::resolver::iterator();
-                //            ++i)
-                //{
-                    //boost::asio::ip::tcp::endpoint end = *i;
-                    //std::cout << end.address() << ' ';
-                //}
-                
+
                 //attempt to open connection
                 tcp::iostream stream(reg->dest_list[reg->dest], "6283");
                 if (!stream)
