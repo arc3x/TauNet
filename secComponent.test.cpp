1b56c9cfdd701b2ec829b0204ea00d0ae634c091
Matthew Slocum <arco000@gmail.com>
2015-12-08 05:10:31 +0000

    added test vectors

--- secComponent.test.cpp
+++ secComponent.test.cpp
@@ -55,6 +55,7 @@ int main(void) {
     //
     //'Unit' test for the encryption / decryption protocols
     //
+    cout << endl << "Unit Tests:" << endl;
     
     //test 1
     //encrypt+decrypt on a single letter message
@@ -123,8 +124,8 @@ int main(void) {
         cout << "secComponent.test Test 6 failed" << endl;
     }
     
-    //test 6
-    //encrypt+decrypt on a multi line message with special characters
+    //test 7
+    //encrypt+decrypt on a multi line message with special characters 
     zero(ciphertext,1000);
     zero(plaintext,1000);
     message = string("version: 0.2\r\n") 
@@ -138,39 +139,119 @@ int main(void) {
     } else {
         cout << "secComponent.test Test 7 failed" << endl;
     }
-    
-    /*
-    XXX WIP
+       
+
     //
-    //Decrytion tests using known encrypted values
+    //Decrytion tests using known good encrypted values
     //
-    cout << "Decryption tests using known good values: " << endl;
+    cout << endl << "Decryption tests using known good values: " << endl;
     
-    //test 7
-    //decrypt cstest1.cs1 
+    //test 8
+    //decrypt tst/cstest1.cs1 
     //Key "asdfg".
     //Plaintext will be "This is a test of CipherSaber." with no newline. 
     zero(ciphertext,1000);
     zero(plaintext,1000);
     key="asdfg";
+    string check = "This is a test of CipherSaber.";
     ifstream cipherFile;
-    cipherFile.open("tst/cstest1.cs1");
-    
+    cipherFile.open("tst/cstest1.cs1");  
     int i=0;
-    char ciphertext2 [1000];
     if (cipherFile.is_open()) {
         while (!cipherFile.eof()) {
-            //cipherFile >> ciphertext;
             ciphertext[i] = cipherFile.get();
-            ciphertext2[i++] = cipherFile.get();
-        }
-        cout<<ciphertext << endl << endl;
-        cout<<ciphertext2;
+            i++;
+        }       
+    }
+    cipherFile.close();            
+    sec->decrypt(ciphertext, i-1, key, 1, plaintext);
+    string out;
+    for (int k=0; k<i-11; k++) {
+        out+=plaintext[k];
+    }        
+    if(out==check) {
+        cout << "Pass" << endl;
+    } else {
+        cout << "secComponent.test Test 8 failed" << endl;
+    }
+    
+    //test 9
+    //decrypt tst/cstest.cs2 
+    //Key "asdfg".
+    //Plaintext will be "This is a test of CipherSaber-2." with no newline. 
+    zero(ciphertext,1000);
+    zero(plaintext,1000);
+    key="asdfg";
+    check = "This is a test of CipherSaber-2.";
+    cipherFile.open("tst/cstest.cs2");  
+    i=0;
+    if (cipherFile.is_open()) {
+        while (!cipherFile.eof()) {
+            ciphertext[i] = cipherFile.get();
+            i++;
+        }       
+    }
+    cipherFile.close();            
+    sec->decrypt(ciphertext, i-1, key, 10, plaintext);
+    out="";
+    for (int k=0; k<i-11; k++) {
+        out+=plaintext[k];
+    }        
+    if(out==check) {
+        cout << "Pass" << endl;
+    } else {
+        cout << "secComponent.test Test 9 failed" << endl;
+    }
+    
+    
+    //test 10
+    //decrypt cipher knight certificate
+    //key ThomasJefferson
+    key = "ThomasJefferson";
+    char crypto_cert[20474];
+    char plain_cert[20474];
+    fstream cert;
+    //read in the crypto file
+    cipherFile.open("tst/cknight.cs1");  
+    i=0;
+    if (cipherFile.is_open()) {
+        while (!cipherFile.eof()) {
+            crypto_cert[i] = cipherFile.get();
+            i++;
+        }       
     }
     cipherFile.close();
-    sec->decrypt(ciphertext, message.length()+10, key, r, plaintext);
-    */
-    
+    //decrypt it
+    sec->decrypt_long(crypto_cert, i-1, key, 1, plain_cert);
+    //write it out
+    cert.open("tst/cknight.gif", fstream::out);
+    if(cert.is_open()) {
+        for(int k=0; k<i-1; k++) {
+                cert << plain_cert[k];
+        }        
+    }       
+    //Read in known good value
+    ifstream trueCert;
+    trueCert.open("tst/cknight.gif.ref");
+    int flag = 0;
+    i = 0;
+    unsigned char c;
+    if (trueCert.is_open()) {
+        while (!trueCert.eof()) {
+            c = trueCert.get();
+            if(plain_cert[i] != c) {
+                flag = 1;
+            }            
+            i++;
+        }       
+    }
+    trueCert.close();
+    //if the dont match fail the test. if they do then pass pass it
+    if(flag==1) {
+        cout << "Pass" << endl;
+    } else {
+        cout << "secComponent.test Test 10 failed" << endl;
+    }
     
     return 0;
 }
